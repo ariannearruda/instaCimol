aecc84a34093231ced2507a801c7f4f4c17a9a07
EclySolar <igorgabrielmorgenstern@gmail.com>
2026-05-14 10:29:51 -0300

    sem ponto e virgula, agr tem

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,16 +7,12 @@ const auth = require("../middleware/auth");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
-// posts
 router.post("/create", auth, upload.single("media"), postController.createPost);
 
-// like
 router.post("/like/:id", auth, postController.likePost);
 
-// comment
 router.post("/comment/:id", auth, postController.commentPost);
 
-// delete
 router.post("/delete/:id", auth, postController.deletePost);
 
 module.exports = router;
