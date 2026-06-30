a3906e2a98e108feb9f165b9aeff8957fad01857
ariannearruda <ariannearruda07@gmail.com>
2026-06-30 15:44:16 -0300

    Remove .env do repositório

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 //app.js
 
-=======
->>>>>>> 8453739ba698cce02a46d1af7502f2cc16f55cf6
 const express = require("express");
 require("dotenv").config();
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
 
-<<<<<<< HEAD
-=======
-const connectDB = require("./config/db");
->>>>>>> 8453739ba698cce02a46d1af7502f2cc16f55cf6
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -21,15 +14,9 @@ const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
     info: {
-<<<<<<< HEAD
       title: "loja API",
       version: "2.0.0",
       description: "Documentação da API da loja"
-=======
-      title: "instaCimol API",
-      version: "1.0.0",
-      description: "Documentação da API da rede social instaCimol"
->>>>>>> 8453739ba698cce02a46d1af7502f2cc16f55cf6
     },
     servers: [
       {
@@ -50,7 +37,6 @@ app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-<<<<<<< HEAD
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/api/status", (req, res) => {
@@ -66,15 +52,6 @@ app.use("/produtos", require("./routes/produtoRoutes"));
 app.use("/clientes", require("./routes/clienteRoutes"));
 app.use("/pedidos", require("./routes/pedidoRoutes"));
 // app.use("/posts", require("./routes/postRoutes"));
-=======
-connectDB();
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.use("/auth", require("./routes/authRoutes"));
-app.use("/posts", require("./routes/postRoutes"));
-
->>>>>>> 8453739ba698cce02a46d1af7502f2cc16f55cf6
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -87,10 +64,7 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-<<<<<<< HEAD
 /*
-=======
->>>>>>> 8453739ba698cce02a46d1af7502f2cc16f55cf6
 const auth = require("./middleware/auth");
 
 app.get("/feed", auth, async (req, res) => {
@@ -102,17 +76,10 @@ app.get("/feed", auth, async (req, res) => {
     user: req.user
   });
 });
-<<<<<<< HEAD
 */
-=======
->>>>>>> 8453739ba698cce02a46d1af7502f2cc16f55cf6
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 8453739ba698cce02a46d1af7502f2cc16f55cf6
